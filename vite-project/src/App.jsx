3eb7fc9831efb54d0d358d98fe579775bbbbe3e0
smriti-bk09 <smritipompom09@gmail.com>
2026-02-23 12:09:46 +0545

    Add Register component to App

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,13 @@
 import Login from "./component/login/Login";
+import Register from "./component/register/Register";
+
 
 function App(){
   return(
     <div>
+      
       <Login />
+      <Register />
     </div>
   );
 }
